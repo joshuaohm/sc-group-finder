d94ae80b84ac29f70eb044e3e6306aabc46b6e13
Joshua Ohm <j.ohm@travispattern.com>
2019-11-18 16:57:36 -0800

    Added axios to entire vue app.

--- client/app.js
+++ client/app.js
@@ -3,6 +3,13 @@ import { sync } from 'vuex-router-sync'
 import App from './components/App'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
 
 sync(store, router)
 
