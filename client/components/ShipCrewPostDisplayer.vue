7b126b42e247a3b9bbb8e8ded6460e6dbbcd0e57
Joshua Ohm <j.ohm@travispattern.com>
2019-11-25 11:50:53 -0800

    createContent() function in SCPost Displayer is broken, need to fix.

--- client/components/ShipCrewPostDisplayer.vue
+++ client/components/ShipCrewPostDisplayer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="SCPostDisplayer-wrapper">
-    <slot v-for="post in this.content.content">
-      <Tab :content="createContent(tab1data, post)"></Tab>
+    <slot>
+      <Tab v-for="(post, index) in this.content.content" :content="this.content.content[index]" :id="'post-'+post.id+'-('+post.ship.id+')'" :key="index"></Tab>
     </slot>
   </div>
 </template>
@@ -23,7 +23,7 @@ export default {
         contentType: 'Tab',
         alignType: 'left',
         lightTheme: false,
-        text: [{ value: "post.slotsAvailable + ' -  '" }, { value: "post.ship.manufacturer + ' ' + post.ship.name" }],
+        text: [{ value: "post.slotsAvailable" }, { value: "post.ship.manufacturer + ' ' + post.ship.name" }, {value: "post.description.substring(0, 20)"}],
         subPanel: {
           contentType: 'SubPanel',
           lightTheme: false,
@@ -37,13 +37,31 @@ export default {
       }
     };
   },
+  beforeMount (){
+
+    for(var i = 0; i < this.content.content.length; i++){
+      console.log(i);
+      var data = this.content.content[i];
+      console.log(data);
+      this.content.content[i] = this.createContent(this.tab1data, data, i);
+    }
+
+
+  },
   methods: {
-    createContent(template, post) {
+    createContent(orig, post, index) {
+
+      var template = [...orig];
+      if(index > 1)
+        console.log('here');
+
       Object.keys(template).forEach(key => {
-        if (typeof template[key] === 'string' && template[key].includes('post.')) {
+        if (typeof(template[key]) === 'string' && template[key].includes('post.')) {
+          console.log(typeof(template[key]))
           template[key] = eval(template[key]);
-        } else if (typeof template[key] === 'object') {
-          this.createContent(template[key], post);
+          console.log(template[key]);
+        } if (typeof template[key] === 'object') {
+          this.createContent(template[key], post, index);
         }
       });
 
@@ -53,5 +71,6 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
 </style>
